f90ade3bba903bb480c4b600a1831814826feb6f
Eric Morancho = Vue <76114369+Hildart@users.noreply.github.com>
2022-11-15 13:58:23 +0100

    Add files via upload

--- src/films.js
+++ src/films.js
@@ -1,23 +1,34 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result =  ???;
+  let result =  array.map(movie => movie.director);
   console.log("EXERCICE 1 ->", result);
   return result;
 }
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
- 
+ let result = array.filter(person => person.director === director);
+ console.log("EXERCICE 2 ->", result);
+ return result;
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
-  
+  let arrayDirector = getMoviesFromDirector(array, director);
+  let arrayScore = arrayDirector.map(movie => movie.score);
+  let scoreSum = arrayScore.reduce((previous, current) => previous + current);
+  let average = parseFloat((scoreSum / arrayScore.length).toFixed(2));
+  console.log("EXERCICE 3 ->", average);
+  return average;
 }
 
 // Exercise 4:  Alphabetic order by title 
 function orderAlphabetically(array) {
-  
+  let films = array.map(titulo => titulo.title);
+  let namesOrder = films.sort();
+  let result = namesOrder.slice(0, 20);
+  console.log("EXERCICE 3 ->", result);
+  return result;
 }
 
 // Exercise 5: Order by year, ascending
@@ -56,3 +67,5 @@ if (typeof module !== 'undefined') {
     bestFilmOfYear,
   };
 }
+
+//orderAlphabetically(movies);
